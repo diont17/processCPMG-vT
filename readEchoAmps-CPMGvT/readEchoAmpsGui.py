83d8d424f04e3716988d168ccde2d5c626a98f21
Dion <dion.thomas@vuw.ac.nz>
2017-05-08 15:59:26 +1200

    Finished makeTestData.py using equations from Silvennoin and Pike Added existing readEchoAmp gui script

---
+++ readEchoAmps-CPMGvT/readEchoAmpsGui.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'REA.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_REAWindow(object):
+    def setupUi(self, REAWindow):
+        REAWindow.setObjectName(_fromUtf8("REAWindow"))
+        REAWindow.resize(482, 207)
+        self.verticalLayoutWidget = QtGui.QWidget(REAWindow)
+        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.label = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout_3.addWidget(self.label)
+        self.inFolder = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.inFolder.setText(_fromUtf8(""))
+        self.inFolder.setObjectName(_fromUtf8("inFolder"))
+        self.horizontalLayout_3.addWidget(self.inFolder)
+        self.btnSelectFolder = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.btnSelectFolder.setObjectName(_fromUtf8("btnSelectFolder"))
+        self.horizontalLayout_3.addWidget(self.btnSelectFolder)
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+        self.horizontalLayout_4 = QtGui.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
+        self.label_3 = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.horizontalLayout_4.addWidget(self.label_3)
+        self.outFile = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.outFile.setObjectName(_fromUtf8("outFile"))
+        self.horizontalLayout_4.addWidget(self.outFile)
+        self.btnSelectFile = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.btnSelectFile.setObjectName(_fromUtf8("btnSelectFile"))
+        self.horizontalLayout_4.addWidget(self.btnSelectFile)
+        self.verticalLayout.addLayout(self.horizontalLayout_4)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.lblStatus = QtGui.QLabel(self.verticalLayoutWidget)
+        self.lblStatus.setText(_fromUtf8(""))
+        self.lblStatus.setObjectName(_fromUtf8("lblStatus"))
+        self.horizontalLayout_2.addWidget(self.lblStatus)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.btnQuit = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.btnQuit.setObjectName(_fromUtf8("btnQuit"))
+        self.horizontalLayout_2.addWidget(self.btnQuit)
+        self.btnLoad = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.btnLoad.setObjectName(_fromUtf8("btnLoad"))
+        self.horizontalLayout_2.addWidget(self.btnLoad)
+        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setEnabled(False)
+        self.pushButton.setFlat(False)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.horizontalLayout_2.addWidget(self.pushButton)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        REAWindow.setCentralWidget(self.verticalLayoutWidget)
+
+        self.retranslateUi(REAWindow)
+        QtCore.QMetaObject.connectSlotsByName(REAWindow)
+
+    def retranslateUi(self, REAWindow):
+        REAWindow.setWindowTitle(_translate("REAWindow", "Read raw echo data- CPMGvariableTau", None))
+        self.label.setText(_translate("REAWindow", "Input folder", None))
+        self.btnSelectFolder.setText(_translate("REAWindow", "Select Folder", None))
+        self.label_3.setText(_translate("REAWindow", "Output file", None))
+        self.btnSelectFile.setText(_translate("REAWindow", "Save", None))
+        self.btnQuit.setText(_translate("REAWindow", "Quit", None))
+        self.btnLoad.setText(_translate("REAWindow", "Load", None))
+        self.pushButton.setText(_translate("REAWindow", "Process", None))
+
---
+++ testData/makeTestData.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 07 12:24:11 2017
+
+@author: dion-
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(15123)
+
+def makeLMdata(echoTime,R0,Tex,K0):
+    """Calculates expected relaxivities following equation (1) from Stefanovic & Pike 2004.
+    echoTime in us
+    R0 in 1/s
+    Tex in s
+    K0 in Tesla squared
+    """
+    gamma=2.675e8 # rad/s/T
+    echoTime=echoTime
+    r2=R0 + (gamma**2 * K0 * Tex) *(1- 2*(Tex/echoTime)*np.tanh(echoTime/(2*Tex)))
+    return r2
+
+ET=1e-3*np.array([2, 2.5, 3, 3.74, 4, 4.5, 5, 7, 10, 12, 14, 17, 20, 30, 40])
+
+#Values of T20, K from Table 1 Stefanovic and Pike 2004
+
+r94 = makeLMdata(ET, 1/0.198, 3e-3, 0.5e-14) #Y=94%: T20=198ms, K=0.5e-14
+r87 = makeLMdata(ET, 1/0.197, 3e-3, 1.4e-14) #Y=87%: T20=197ms, K=1.4e-14 
+r72 = makeLMdata(ET, 1/0.200, 3e-3, 2.9e-14) #Y=72%: T20=200ms, K=2.9e-14
+r66 = makeLMdata(ET, 1/0.183, 3e-3, 3.7e-14) #Y=66%: T20=183ms, K=3.7e-14
+r62 = makeLMdata(ET, 1/0.184, 3e-3, 4.6e-14) #Y=62%: T20=184ms, K=4.6e-14
+r42 = makeLMdata(ET, 1/0.166, 3e-3, 9.4e-14) #Y=42%: T20=166ms, K=9.4e-14
+
+plt.plot(1e3*ET, r94, label="Y=0.94")
+plt.plot(1e3*ET, r87, label="Y=0.87")
+plt.plot(1e3*ET, r72, label="Y=0.72")
+plt.plot(1e3*ET, r66, label="Y=0.66")
+plt.plot(1e3*ET, r62, label="Y=0.62")
+plt.plot(1e3*ET, r42, label="Y=0.42")
+plt.xlabel("Echo Time (ms)")
+plt.ylabel("R2 (s-1)")
+plt.legend()
+
+#build echo decays
+nrEchoes=32
+snr=6
+
+plt.figure()
+
+decayX=np.ndarray((ET.shape[0],nrEchoes))
+decayY=np.ndarray((ET.shape[0],nrEchoes))
+
+echoes=np.zeros((ET.shape[0],nrEchoes,32), dtype=np.complex)
+echotimeaxis=np.arange(32)
+
+for i in range(len(ET)):
+    decayX[i]=np.arange(nrEchoes)*ET[i]
+    decayY[i]=np.exp(-1*r94[i]*decayX[i])
+    plt.plot(decayX[i],decayY[i])
+    
+    for j in range(nrEchoes):
+        #Gaussian echo + noise:
+        echoes[i,j]=decayY[i,j]*snr*np.exp(-0.01*(64-echotimeaxis)**2 )+ np.random.rand(echotimeaxis.shape[0]) + 1j*np.random.rand(echotimeaxis.shape[0])
+    np.savetxt('r94/ET%1d.csv'%i, echoes[i],delimiter=',')
+np.savetxt('r94/echoTimes.csv',ET)
+    
